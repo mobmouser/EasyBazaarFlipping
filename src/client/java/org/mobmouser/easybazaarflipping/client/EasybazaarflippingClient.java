10eb18df2a0f1a7dac194702e4b491d5a557cde8
mobmouser <1018ray8128@gmail.com>
2026-02-15 21:41:32 +0900

    コマンド登録忘れていたため追加

--- src/client/java/org/mobmouser/easybazaarflipping/client/EasybazaarflippingClient.java
+++ src/client/java/org/mobmouser/easybazaarflipping/client/EasybazaarflippingClient.java
@@ -14,5 +14,6 @@ public class EasybazaarflippingClient implements ClientModInitializer {
         GetPurse.initUuid();
         ChatListener.register();
         NpcSellCounter.init();
+        CommandLoader.register();
     }
 }
